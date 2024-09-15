e4dcdfb3325b8fe4e52ac893b75c61377c36e587
Aadarsh Singh <aadarsh4492@gmail.com>
2024-09-15 23:22:35 +0530

    Added star components

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -5,7 +5,7 @@ class MovieCard extends React.Component {
             < div className="main" >
                 < div className="movie-card" >
                     < div className="left" >
-                        <img alt="Movie Poster" />
+                        <img alt="Movie Poster" src="" />
                     </div >
                     < div className="right" >
                         < div className="title" >The Avengers</div >
@@ -13,7 +13,12 @@ class MovieCard extends React.Component {
                         < div className="plot" >$199.00</div >
                         < div className="footer" >
                             < div className="rating" >8.9</div >
-                            < div className="stars" >star</div >
+                            < div className="star-dis" >
+                                <img alt="star" className="str-btn" src="https://cdn-icons-png.flaticon.com/128/1828/1828901.png" />
+                                <img alt="star" className="stars" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" />
+                                <img alt="star" className="str-btn" src="https://cdn-icons-png.flaticon.com/128/20/20183.png" />
+                                <span>0</span>
+                            </div >
                             <button className="favourite-btn">Favourite</button>
                             <button className="cart-btn">Add to Cart</button>
                         </div >
